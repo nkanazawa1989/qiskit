8a8ea7e815ec8fe06b02cd7ac05f1b233af2ec13
Toshinari Itoko <itoko@jp.ibm.com>
2019-04-04 16:17:28 +0900

    simplify reference schedule

--- qiskit/test/reference_pulses.py
+++ qiskit/test/reference_pulses.py
@@ -17,34 +17,23 @@ class ReferenceSchedules:
     @staticmethod
     def nonsense():
         """Return a nonsense schedule just for tests."""
-        @pulse.functional_pulse
-        def gaussian(duration, amp, t0, sig):
-            x = np.linspace(0, duration - 1, duration)
-            return amp * np.exp(-(x - t0) ** 2 / sig ** 2)
 
-        gp0 = gaussian(duration=20, name='pulse0', amp=0.7, t0=9.5, sig=3)
-        gp1 = gaussian(duration=20, name='pulse1', amp=0.5, t0=9.5, sig=3)
+        @pulse.functional_pulse
+        def linear(duration: int):
+            x = np.arange(0, duration)
+            return 0.2 * x + 0.1
+
+        lp0 = linear(duration=3, name='pulse0')
 
         qubits = [
             pulse.channels.Qubit(0,
-                                 drive_channels=[pulse.channels.DriveChannel(0, 1.2)],
-                                 control_channels=[pulse.channels.ControlChannel(0)]),
-            pulse.channels.Qubit(1,
-                                 drive_channels=[pulse.channels.DriveChannel(1, 3.4)],
-                                 acquire_channels=[pulse.channels.AcquireChannel(1)])
+                                 drive_channels=[pulse.channels.DriveChannel(0)],
+                                 acquire_channels=[pulse.channels.AcquireChannel(0)]),
         ]
-        registers = [pulse.channels.RegisterSlot(i) for i in range(2)]
+        registers = [pulse.channels.RegisterSlot(i) for i in range(1)]
         device = pulse.DeviceSpecification(qubits, registers)
 
-        fc_pi_2 = pulse.FrameChange(phase=1.57)
-        acquire = pulse.Acquire(10)
         sched = pulse.Schedule()
-        sched.insert(0, gp0(device.q[0].drive))
-        sched.insert(0, pulse.PersistentValue(value=0.2+0.4j)(device.q[0].control))
-        sched.insert(30, gp1(device.q[1].drive))
-        sched.insert(60, pulse.FrameChange(phase=-1.57)(device.q[0].drive))
-        sched.insert(60, gp0(device.q[0].control))
-        sched.insert(80, pulse.Snapshot("label", "snap_type"))
-        sched.insert(90, fc_pi_2(device.q[0].drive))
-        sched.insert(90, acquire(device.q[1], device.mem[1], device.c[1]))
+        sched.insert(0, lp0(device.q[0].drive))
+        sched.insert(3, pulse.Acquire(10)(device.q[0], device.mem[0]))
         return sched
